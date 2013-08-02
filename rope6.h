cbb751dc625ab4dc8e87e17e1a2c397f5d0c2180
Heng Li <lh3@me.com>
2013-08-02 14:19:18 -0400

    fixed a typo in decoding

--- rope6.h
+++ rope6.h
@@ -7,16 +7,10 @@ static inline int rle_dec(const uint8_t *p, int *c, int64_t *l)
 { // FIXME: little-endian ONLY!!!
 	const uint64_t rle_const = 0x232235314C484440ULL;
 	const uint64_t *q = (const uint64_t*)p;
-	int d = rle_const >> (*p>>5<<2) & 0xff, n_bytes = 1 << (d & 0x3);
+	int d = rle_const >> (*p>>5<<3) & 0xff, n_bytes = 1 << (d & 0x3);
 	*c = *p & 0x7;
 	*l = (*q>>8 & ((1ULL << ((n_bytes-1)<<3)) - 1)) << (d>>4) | (d & 0xc) | (*p>>3 & 0x3);
 	return n_bytes;
-}
-
-static inline int rle_enc1(uint8_t *p, int c)
-{
-	*p = 1<<3 | c;
-	return 1;
 }
 
 static inline int rle_enc(uint8_t *p, int c, int64_t l)
