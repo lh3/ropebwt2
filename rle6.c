31ef78c7c4f6684b8320f17cb0424d741ec95d2b
Heng Li <lh3@me.com>
2013-08-08 16:48:58 -0400

    only keep the 43+3 codec

--- rle6.c
+++ rle6.c
@@ -9,11 +9,9 @@
 #define LIKELY(x) (x)
 #endif
 
-#if RLE_CODEC == 1 || RLE_CODEC == 4
-
-/**************************
- *** 58+3 or 43+3 codec ***
- **************************/
+/******************
+ *** 43+3 codec ***
+ ******************/
 
 // insert symbol $a after $x symbols in $str; marginal counts added to $cnt; returns the size increase
 int rle_insert_core(int len, uint8_t *str, int64_t x, int a, int64_t rl, int64_t cnt[6], int *m_bytes)
@@ -26,12 +24,6 @@ int rle_insert_core(int len, uint8_t *str, int64_t x, int a, int64_t rl, int64_t
 		int64_t pre, z = 0, l = 0;
 		int c = -1, n_bytes = 0, n_bytes2;
 		uint8_t tmp[24];
-#if RLE_CODEC == 1
-		while (z < x) {
-			n_bytes = rle_dec(p, &c, &l);
-			z += l; p += n_bytes; cnt[c] += l;
-		}
-#else
 		while (z < x) {
 			q = p;
 			c = *p & 7;
@@ -50,7 +42,6 @@ int rle_insert_core(int len, uint8_t *str, int64_t x, int a, int64_t rl, int64_t
 			z += l; cnt[c] += l;
 		}
 		n_bytes = p - q;
-#endif
 		if (x == z && a != c && p < end) { // then try the next run
 			int t_bytes, tc;
 			int64_t tl;
@@ -138,241 +129,3 @@ void rle_print(int block_len, const uint8_t *block)
 	}
 	putchar('\n');
 }
-
-#elif RLE_CODEC == 2
-
-/*****************
- *** 5+3 codec ***
- *****************/
-
-int rle_insert1(int block_len, uint8_t *block, int64_t x, int a, int64_t r[6], const int64_t c[6])
-{ // IMPORTANT: always assume rl == 1
-#define MAX_RUNLEN 31
-#define _insert_after(_n, _s, _i, _b) if ((_i) + 1 != (_n)) memmove(_s+(_i)+2, _s+(_i)+1, (_n)-(_i)-1); _s[(_i)+1] = (_b); ++(_n)
-
-	uint32_t *nptr = (uint32_t*)(block + block_len - 4);
-	int i, len, l = 0, n = *nptr;
-	uint8_t *s = block;
-
-	if (n == 0) { // if $s is empty, that is easy
-		for (i = 0; i < 6; ++i) r[i] = 0;
-		s[n++] = 1<<3 | a;
-		*nptr = n;
-		return 0;
-	}
-	len = c[0] + c[1] + c[2] + c[3] + c[4] + c[5];
-	if (x < len>>1) { // forwardly search for the run to insert
-		for (i = 0; i < 6; ++i) r[i] = 0;
-		do {
-			l += *s>>3;
-			r[*s&7] += *s>>3;
-			++s;
-		} while (l < x);
-	} else { // backwardly search for the run to insert; this block has exactly the same functionality as the above
-		for (i = 0; i < 6; ++i) r[i] = c[i];
-		l = len, s += n;
-		do {
-			--s;
-			l -= *s>>3;
-			r[*s&7] -= *s>>3;
-		} while (l >= x);
-		l += *s>>3; r[*s&7] += *s>>3; ++s;
-	}
-	i = s - block; s = block;
-	assert(i <= n);
-	r[s[--i]&7] -= l - x; // $i now points to the left-most run where $a can be inserted
-	if (l == x && i != n - 1 && (s[i+1]&7) == a) ++i; // if insert to the end of $i, check if we'd better to the start of ($i+1)
-	if ((s[i]&7) == a) { // insert to a long $a run
-		if (s[i]>>3 == MAX_RUNLEN) { // the run is full
-			for (++i; i != n && (s[i]&7) == a; ++i); // find the end of the long run
-			--i;
-			if (s[i]>>3 == MAX_RUNLEN) { // then we have to add one run
-				_insert_after(n, s, i, 1<<3|a);
-			} else s[i] += 1<<3;
-		} else s[i] += 1<<3;
-	} else if (l == x) { // insert to the end of run; in this case, neither this and the next run is $a
-		_insert_after(n, s, i, 1<<3 | a);
-	} else if (i != n - 1 && (s[i]&7) == (s[i+1]&7)) { // insert to a long non-$a run
-		int rest = l - x, c = s[i]&7;
-		s[i] -= rest<<3;
-		_insert_after(n, s, i, 1<<3 | a);
-		for (i += 2; i != n && (s[i]&7) == c; ++i); // find the end of the long run
-		--i;
-		if ((s[i]>>3) + rest > MAX_RUNLEN) { // we cannot put $rest to $s[$i]
-			rest = (s[i]>>3) + rest - MAX_RUNLEN;
-			s[i] = MAX_RUNLEN<<3 | (s[i]&7);
-			_insert_after(n, s, i, rest<<3 | c);
-		} else s[i] += rest<<3;
-	} else { // insert to a short run
-		memmove(s + i + 3, s + i + 1, n - i - 1);
-		s[i]  -= (l-x)<<3;
-		s[i+1] = 1<<3 | a;
-		s[i+2] = (l-x)<<3 | (s[i]&7);
-		n += 2;
-	}
-	*nptr = n;
-	return n + 2 > block_len - 4? 1 : 0;
-
-#undef MAX_RUNLEN
-#undef _insert_after
-}
-
-void rle_split(int block_len, uint8_t *block, uint8_t *new_block)
-{
-	uint32_t *nptr = (uint32_t*)(block + block_len - 4);
-	int beg = *nptr >> 1;
-	memcpy(new_block, block + beg, (*nptr) - beg);
-	*(uint32_t*)(new_block + block_len - 4) = *nptr - beg;
-	*nptr = beg;
-}
-
-void rle_count(int block_len, const uint8_t *block, int64_t cnt[6])
-{
-	uint32_t *nptr = (uint32_t*)(block + block_len - 4);
-	const uint8_t *p = block, *end = p + (*nptr);
-	for (; p != end; ++p) cnt[*p&7] += *p>>3;
-}
-
-#elif RLE_CODEC == 3
-
-/******************
- *** 11+3 codec ***
- ******************/
-
-static inline uint8_t insert_aux(uint8_t *b, uint8_t *e, int l)
-{
-	l += *b >> 3;
-	*b = (l&0xf) << 3 | (*b&7);
-	l >>= 4;
-	if (e == 0) return l? 0x80 | l : 0;
-	if (*e + l < 0x100) {
-		*e += l;
-		return 0;
-	} else {
-		uint8_t u = l - (0xff - *e);
-		*e = 0xff;
-		return 0x80 | u;
-	}
-}
-// $b points to the first run; if $e==0, a short run; otherwise $e points to the last run; $iptr is the insertion point; $l is the length to insert
-static inline int insert_aux2(uint8_t *b, uint8_t *e, uint8_t *iptr, uint8_t *end, int l)
-{
-	uint8_t u;
-	u = insert_aux(b, e, l);
-	if (u) {
-		memmove(iptr + 1, iptr, end - iptr);
-		*iptr = u;
-		return 1;
-	} else return 0;
-}
-
-int rle_insert1(int block_len, uint8_t *block, int64_t x, int a, int64_t r[6], const int64_t c[6])
-{
-	uint32_t *nptr = (uint32_t*)(block + block_len - 4);
-	int64_t tot, l;
-	uint8_t *end, *p, *q, *e;
-
-	memset(r, 0, 48);
-	if (*nptr == 0) { // an empty block, that is easy
-		*block = 1<<3 | a, *nptr = 1;
-		return 0;
-	}
-
-	end = block + *nptr;
-	tot = c[0] + c[1] + c[2] + c[3] + c[4] + c[5];
-	if (x < tot>>1) { // forward search for the run to insert
-		int b = 0, t;
-		l = 0, p = block;
-		do {
-			if (*p>>7) t = (*p&0x7f) << 4;
-			else t = *p >> 3, b = *p & 7;
-			r[b] += t; l += t;
-			++p;
-		} while (l < x);
-		q = p - 1;
-		for (p = q; *p>>7; --p);
-	} else { // backward search for the run to insert
-		int b = 0, t = 0;
-		memcpy(r, c, 48);
-		l = tot, p = end;
-		do {
-			--p;
-			if (*p>>7) t += (*p & 0x7f) << 4;
-			else t += *p>>3, l -= t, r[*p&7] -= t, t = 0;
-		} while (l >= x);
-		q = p;
-		b = *p&7;
-		r[b] += *q >> 3; l += *q++ >> 3;
-		while (l < x) {
-			t = (*q++&0x7f) << 4;
-			r[b] += t; l += t;
-		}
-		--q;
-	}
-	r[*p&7] -= l - x;
-	if (l == x && q < end - 1 && q[1]>>7 == 0 && (q[1]&7) == a) // if the next run is an $a run..
-		l += rle_run_len(q), p = ++q;
-	for (e = q + 1; e < end && *e>>7; ++e); // find the end of the complete run
-	--e;
-
-	if ((*p&7) == a) {
-		end += insert_aux2(p, e == p? 0 : e, e + 1, end, 1);
-	} else {
-		uint8_t buf[5], *s = buf;
-		int w = rle_run_len(q) - (l - x);
-		if (q != p) {
-			*s = insert_aux(p, 0, w);
-			if (*s) ++s;
-		} else *s++ = w<<3 | (*p&7);
-		*s++ = 1<<3 | a;
-		*s = *p&7;
-		end += insert_aux2(s, e == q? 0 : e, e + 1, end, l - x);
-		if (*s>>3 || (q + 1 < end && q[1]>>7)) ++s;
-		if (q < end) memmove(q + (s - buf), q + 1, end - q - 1);
-		end += (s - buf) - 1;
-		memcpy(q, buf, s - buf);
-	}
-
-	*nptr = end - block;
-	return *nptr + 4 > block_len - 4? 1 : 0;
-}
-
-void rle_print(int block_len, const uint8_t *block)
-{
-	int b = 0;
-	const uint8_t *p, *end;
-	end = block + *(uint32_t*)(block + block_len - 4);
-	for (p = block; p < end; ++p) {
-		if (*p>>7) printf("%c%d", "$ACGTN"[b], (*p&0x7f)<<4);
-		else printf("%c%d", "$ACGTN"[b=*p&7], *p>>3);
-	}
-	putchar('\n');
-}
-
-void rle_split(int block_len, uint8_t *block, uint8_t *new_block)
-{
-	uint32_t *nptr = (uint32_t*)(block + block_len - 4);
-	int beg = *nptr >> 1, off = 0;
-	if (block[beg]>>7) {
-		uint8_t *p;
-		for (p = block + beg - 1; *p>>7; --p);
-		new_block[0] = *p&7;
-		off = 1;
-	}
-	memcpy(new_block + off, block + beg, (*nptr) - beg);
-	*(uint32_t*)(new_block + block_len - 4) = *nptr - beg + off;
-	*nptr = beg;
-}
-
-void rle_count(int block_len, const uint8_t *block, int64_t cnt[6])
-{
-	uint32_t *nptr = (uint32_t*)(block + block_len - 4);
-	const uint8_t *p, *end = block + (*nptr);
-	int b;
-	for (p = block, b = 0; p != end; ++p) {
-		if (*p>>7) cnt[b] += (*p&0x7f) << 4;
-		else b = *p&7, cnt[b] += *p>>3;
-	}
-}
-
-#endif
--- rle6.h
+++ rle6.h
@@ -15,73 +15,6 @@ extern "C" {
 }
 #endif
 
-#define RLE_CODEC 4
-
-#if RLE_CODEC == 1 || RLE_CODEC == 4
-
-/**************************
- *** 58+3 or 42+3 codec ***
- **************************/
-
-#if RLE_CODEC == 1
-
-/******************
- *** 58+3 codec ***
- ******************/
-
-#define RLE_CONST 0x232235314C484440ULL
-#define rle_bytes(_p) (1 << (RLE_CONST >> (*(_p)>>5<<3) & 3))
-#define rle_runs(len, block) (*(uint32_t*)((block) + (len) - 4) >> 4)
-/*
-static inline int rle_dec(const uint8_t *p, int *c, int64_t *l)                                                                                                       
-{
-	const uint64_t x = *(const uint64_t*)p;
-	int d = RLE_CONST >> (x>>5<<3) & 0xff, n_bytes = 1 << (d & 0x3);
-	*c = x & 0x7;
-	*l = (x & ((1ULL << (n_bytes<<3)) - 1)) >> 8 << (d>>4) | (d & 0xc) | (x>>3 & 0x3);
-	return n_bytes;
-}
-*/
-static inline int rle_dec(const uint8_t *p, int *c, int64_t *l)                                                                                                       
-{
-	*c = *p & 0x7;
-	if ((*p&0x80) == 0) {
-		*l = *p >> 3;
-		return 1;
-	} else if ((*p&0xC0) == 0x80) {
-		*l = (uint64_t)p[1] << 3 | (*p >> 3 & 0x7);
-		return 2;
-	} else if ((*p&0xE0) == 0xC0) {
-		*l = *(uint32_t*)p >> 8 << 2 | (*p >> 3 & 0x3);
-		return 4;
-	} else {
-		*l = *(uint64_t*)p >> 8 << 2 | (*p >> 3 & 0x3);
-		return 8;
-	}
-}
-
-static inline int rle_enc(uint8_t *p, int c, int64_t l)
-{ // FIXME: little-endian ONLY!!!
-	if (l < 1LL<<4) {
-		*p = l<<3 | c;
-		return 1;
-	} else if (l < 1LL<<11) {
-		uint16_t *q = (uint16_t*)p;
-		*q = c | (l&7)<<3 | 0x80 | l>>3<<8;
-		return 2;
-	} else if (l < 1LL<<26) {
-		uint32_t *q = (uint32_t*)p;
-		*q = c | (l&3)<<3 | 0xC0 | l>>2<<8;
-		return 4;
-	} else {
-		uint64_t *q = (uint64_t*)p;
-		*q = c | (l&3)<<3 | 0xE0 | l>>2<<8;
-		return 8;
-	}
-}
-
-#else
-
 /******************
  *** 43+3 codec ***
  ******************/
@@ -132,24 +65,5 @@ static inline int rle_enc(uint8_t *p, int c, int64_t l)
 		return 8;
 	}
 }
-
-#endif
-
-#elif RLE_CODEC == 2
-
-#define rle_runs(len, block) (*(uint32_t*)((block) + (len) - 4))
-
-static inline int rle_dec(const uint8_t *p, int *c, int64_t *l)
-{
-	*c = *p&7; *l = *p>>3;
-	return 1;
-}
-
-#elif RLE_CODEC == 3
-
-#define rle_runs(len, block) (*(uint32_t*)((block) + (len) - 4))
-#define rle_run_len(p) ((*p)>>7? ((*p)&0x7f)<<4 : (*p)>>3)
-
-#endif
 
 #endif
