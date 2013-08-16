f756bf2f4e402d92022cbfbe8f1cf4ee03e824f5
Heng Li <lh3@me.com>
2013-08-16 14:38:34 -0400

    forward is faster than backward, so more forward

--- rle6.c
+++ rle6.c
@@ -24,7 +24,7 @@ int rle_insert(uint8_t *block, int64_t x, int a, int64_t rl, int64_t cnt[6], con
 		tot = ec[0] + ec[1] + ec[2] + ec[3] + ec[4] + ec[5];
 		if (x == 0) {
 			p = q = block; z = 0;
-		} else if (x <= tot>>1) { // forward
+		} else if (x <= (tot>>1) + (tot>>3)) { // forward
 			z = 0; p = block;
 			while (z < x) {
 				rle_dec1(p, c, l);
@@ -123,7 +123,7 @@ void rle_rank2a(const uint8_t *block, int64_t x, int64_t y, int64_t *cx, int64_t
 	y = y >= x? y : x;
 	tot = ec[0] + ec[1] + ec[2] + ec[3] + ec[4] + ec[5];
 	if (tot == 0) return;
-	if (x <= tot - y) {
+	if (x <= (tot - y) + (tot>>3)) {
 		int c = 0;
 		int64_t l, z = 0;
 		memset(cnt, 0, 48);
