1798814678d8f28dea67c5dbfcb6dd1cf362648b
Heng Li <lh3@me.com>
2013-08-16 10:51:30 -0400

    debugging; not working yet

--- rle6.c
+++ rle6.c
@@ -115,7 +115,7 @@ void rle_print(int block_len, const uint8_t *block, int expand)
 	}
 	putchar('\n');
 }
-
+/*
 void rle_rank1a(int block_len, const uint8_t *block, int64_t x, int64_t cnt[6], const int64_t ec[6])
 {
 	int64_t tot;
@@ -151,7 +151,7 @@ void rle_rank1a(int block_len, const uint8_t *block, int64_t x, int64_t cnt[6], 
 		cnt[*p&7] += x - z;
 	}
 }
-
+*/
 void rle_rank2a(int block_len, const uint8_t *block, int64_t x, int64_t y, int64_t cx[6], int64_t cy[6], const int64_t ec[6])
 {
 	int a;
@@ -159,7 +159,7 @@ void rle_rank2a(int block_len, const uint8_t *block, int64_t x, int64_t y, int64
 	const uint8_t *p;
 
 	tot = ec[0] + ec[1] + ec[2] + ec[3] + ec[4] + ec[5];
-	if (x < tot - y) {
+	if (0 || x < tot - y) {
 		int c = 0;
 		int64_t l, z = 0;
 		memset(cnt, 0, 48);
@@ -204,3 +204,11 @@ void rle_rank2a(int block_len, const uint8_t *block, int64_t x, int64_t y, int64
 #undef move_backward
 	}
 }
+
+void rle_rank1a(int block_len, const uint8_t *block, int64_t x, int64_t cnt[6], const int64_t ec[6])
+{
+	int64_t dummy[6];
+	memset(dummy, 0, 48);
+	rle_rank2a(block_len, block, x, x, cnt, dummy, ec);
+}
+
